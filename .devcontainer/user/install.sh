f21b5f47f637c98a0661f8a14e5d9edd66f4140e
Vasiliy Polyakov <git@invasy.dev>
2023-04-13 18:25:58 +0500

    search: add pagefind

--- .devcontainer/user/install.sh
+++ .devcontainer/user/install.sh
@@ -5,6 +5,11 @@ shopt -qs lastpipe
 : "${USERNAME:=$_CONTAINER_USER}"
 : "${PASSWORD:=$USERNAME}"
 : "${COMMENT:=${USERNAME@u}}"
+: "${ROOT_PASSWORD:=root}"
 
+# Set root password
+yes "$ROOT_PASSWORD" | passwd || :
+
+# Create user and group
 useradd --create-home --comment="$COMMENT" --user-group "$USERNAME"
 yes "$PASSWORD" | passwd "$USERNAME" || :
