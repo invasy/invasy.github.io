0205ec1937e3732cf71be3e9f59bc0b0e5cf68c9
Vasiliy Polyakov <git@invasy.dev>
2023-04-16 01:10:41 +0500

    postCreateCommand: add comments

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -2,9 +2,12 @@
 set -euo pipefail
 shopt -qs lastpipe
 
+# Install Go tools
 mkdir -p "$GOPATH/src" "$GOPATH/pkg" "$GOPATH/bin"
 go install golang.org/x/tools/gopls@latest
 
+# Install required Python packages
 pip3 install -r requirements.txt
 
+# Update Hugo modules
 hugo mod get -u
